fc29db8d265a09926b9d3f10eb0108cd7a51cab9
reefridge <reefridgerator@gmail.com>
2021-01-25 22:22:32 +0700

    chage type names

--- src/main.c
+++ src/main.c
@@ -36,8 +36,8 @@ bool BitArray_test(char* arr, unsigned int idx)
 	return !!(arr[block_idx] & mask);
 }
 
-typedef struct Cell Cell;
-struct Cell
+typedef struct Box Box;
+struct Box
 {
 	Vector2 pos;
 	Vector2 size;
@@ -50,7 +50,7 @@ Rectangle Rect_CreateCentered(Vector2 const pos, Vector2 const size)
 	return rect;
 }
 
-void Cell_draw(Cell const * const self)
+void Box_draw(Box const * const self)
 {
 	Rectangle rect = Rect_CreateCentered(self->pos, self->size);
 	Rectangle border = Rect_CreateCentered(self->pos, Vector2AddValue(self->size, 8.0));
@@ -58,81 +58,81 @@ void Cell_draw(Cell const * const self)
 	DrawRectangleRec(rect, self->color);
 }
 
-Rectangle Cell_getCollisionRect(Cell const * const self)
+Rectangle Box_getCollisionRect(Box const * const self)
 {
 	return Rect_CreateCentered(self->pos, self->size);
 }
 
-typedef struct Cell_Selectable Cell_Selectable;
-struct Cell_Selectable
+typedef struct Cell Cell;
+struct Cell
 {
-	Cell as_Cell;
+	Box as_Box;
 	bool def;
 	bool owned;
 	bool selected;
 	Color selected_color;
 	Color normal_color;
 	Color owned_color;
-	Cell const * attached_cell;
+	Box const * attached_box;
 	Vector2 attached_pos_diff;
 	size_t idx;
 	char * connections;
 };
 
-void Cell_Selectable_ConnectCells(Cell_Selectable * const a, Cell_Selectable * const b)
+void Cell_ConnectCells(Cell * const a, Cell * const b)
 {
 	BitArray_set(a->connections, b->idx);
 	BitArray_set(b->connections, a->idx);
 }
 
-void Cell_Selectable_DisconnectCells(Cell_Selectable * const a, Cell_Selectable * const b)
+void Cell_DisconnectCells(Cell * const a, Cell * const b)
 {
 	BitArray_unset(a->connections, b->idx);
 	BitArray_unset(b->connections, a->idx);
 }
 
-bool Cell_Selectable_AreCellsConnected(Cell_Selectable const * const a, Cell_Selectable const * const b)
+bool Cell_AreCellsConnected(Cell const * const a, Cell const * const b)
 {
 	return BitArray_test(a->connections, b->idx) && BitArray_test(b->connections, a->idx);
 }
 
-void Cell_Selectable_updateColor(Cell_Selectable * const self)
+void Cell_updateColor(Cell * const self)
 {
-	self->as_Cell.color = self->selected ? self->selected_color : (self->owned ? self->owned_color : self->normal_color);
+	self->as_Box.color = self->selected ? self->selected_color : (self->owned ? self->owned_color : self->normal_color);
 }
 
-void Cell_Selectable_setSelected(Cell_Selectable * const self, bool selected)
+void Cell_setSelected(Cell * const self, bool selected)
 {
 	self->selected = selected;
-	Cell_Selectable_updateColor(self);
+	Cell_updateColor(self);
 }
 
-void Cell_Selectable_setOwned(Cell_Selectable * const self, bool owned)
+void Cell_setOwned(Cell * const self, bool owned)
 {
 	self->owned = self->def || owned;
-	Cell_Selectable_updateColor(self);
+	Cell_updateColor(self);
 }
 
-void Cell_Selectable_boundToCell(Cell_Selectable * const self, Cell const * const to)
+void Cell_boundToBox(Cell * const self, Box const * const box)
 {
-	if (self->attached_cell != to)
+	if (self->attached_box != box)
 	{
-		self->attached_cell = to;
-		self->attached_pos_diff = Vector2Subtract(self->as_Cell.pos, to->pos);
+		self->attached_box = box;
+		self->attached_pos_diff = Vector2Subtract(self->as_Box.pos, box->pos);
 	}
 }
 
-void Cell_Selectable_unbound(Cell_Selectable * const self)
+void Cell_unbound(Cell * const self)
 {
-	self->attached_cell = 0;
+	self->attached_box = 0;
 	self->attached_pos_diff = Vector2Zero();
 }
 
-void Cell_Selectable_updatePos(Cell_Selectable * const self)
+void Cell_updatePos(Cell * const self)
 {
-	if (self->attached_cell)
+	if (self->attached_box)
 	{
-		self->as_Cell.pos = Vector2Add(self->attached_cell->pos, self->attached_pos_diff);
+		self->as_Box.pos = Vector2Add(self->attached_box->pos, self->attached_pos_diff);
 	}
 }
 
@@ -144,29 +144,29 @@ Vector2 size = (Vector2) { .x = 50.0, .y = 50.0 };
 
 int main(void)
 {
-	Cell pointer = (Cell) {.pos = point, .size = (Vector2) {.x = 10.0, .y = 10.0}, .color = BLUE};
+	Box pointer = (Box) {.pos = point, .size = (Vector2) {.x = 10.0, .y = 10.0}, .color = BLUE};
 
-	Cell_Selectable cells[CELLS_COUNT];
+	Cell cells[CELLS_COUNT];
 
 	char conns[CELLS_COUNT][FS] = {{0}};
 
 	for (int i = 0; i < CELLS_COUNT; ++i)
 	{
-		cells[i] = (Cell_Selectable) {
-			.as_Cell = (Cell) {.pos = Vector2Add(point, (Vector2) {(i - 7) * 65.0, 0.0}), .size = size, .color = RED},
+		cells[i] = (Cell) {
+			.as_Box = (Box) {.pos = Vector2Add(point, (Vector2) {(i - 7) * 65.0, 0.0}), .size = size, .color = RED},
 			.selected = false,
 			.def = i - 7 == 0,
 			.owned = i - 7 == 0,
 			.selected_color = LIME,
 			.owned_color = ORANGE,
 			.normal_color = RED,
-			.attached_cell = 0,
+			.attached_box = 0,
 			.attached_pos_diff = Vector2Zero(),
 			.idx = i,
 			.connections = conns[i]
 		};
 	}
-	Cell_Selectable * connecting_cell = 0;
+	Cell * connecting_cell = 0;
 
 	InitWindow(WIN_W, WIN_H, "Symbiosis");
 	HideCursor();
@@ -176,7 +176,7 @@ int main(void)
 		Vector2 mouse_pos = GetMousePosition();
 		pointer.pos = mouse_pos;
 
-		Cell_Selectable * selected_cell = 0;
+		Cell * selected_cell = 0;
 		for (int i = 0; i < CELLS_COUNT; ++i)
 		{
 			if (cells[i].selected)
@@ -200,27 +200,27 @@ int main(void)
 				{
 					if (selected_cell->owned)
 					{
-						Cell_Selectable_boundToCell(selected_cell, &pointer);
-						Cell_Selectable_updatePos(selected_cell);
+						Cell_boundToBox(selected_cell, &pointer);
+						Cell_updatePos(selected_cell);
 					}
 				}
 			}
 			else
 			{
-				Cell_Selectable_unbound(selected_cell);
+				Cell_unbound(selected_cell);
 			}
 
 			if (connecting_cell && connecting_cell != selected_cell && (IsKeyReleased(KEY_LEFT_SHIFT) || IsMouseButtonReleased(MOUSE_LEFT_BUTTON)))
 			{
-				if (Cell_Selectable_AreCellsConnected(selected_cell, connecting_cell))
+				if (Cell_AreCellsConnected(selected_cell, connecting_cell))
 				{
-					Cell_Selectable_DisconnectCells(selected_cell, connecting_cell);
+					Cell_DisconnectCells(selected_cell, connecting_cell);
 				}
 				else
 				{
 					if (selected_cell->owned || connecting_cell->owned)
 					{
-						Cell_Selectable_ConnectCells(selected_cell, connecting_cell);
+						Cell_ConnectCells(selected_cell, connecting_cell);
 					}
 				}
 			}
@@ -234,16 +234,16 @@ int main(void)
 		// Update owned state
 		for (int i = 0; i < CELLS_COUNT; ++i)
 		{
-			Cell_Selectable_setOwned(&cells[i], false);
+			Cell_setOwned(&cells[i], false);
 			for (int j = 0; j < CELLS_COUNT; ++j)
 			{
 				if (i == j) continue;
-				if (Cell_Selectable_AreCellsConnected(&cells[i], &cells[j]))
+				if (Cell_AreCellsConnected(&cells[i], &cells[j]))
 				{
 					if (cells[i].owned || cells[j].owned)
 					{
-						Cell_Selectable_setOwned(&cells[i], true);
-						Cell_Selectable_setOwned(&cells[j], true);
+						Cell_setOwned(&cells[i], true);
+						Cell_setOwned(&cells[j], true);
 						break;
 					}
 				}
@@ -253,10 +253,10 @@ int main(void)
 		// Update selected state
 		for (int i = 0; i < CELLS_COUNT; ++i)
 		{
-			bool collision = CheckCollisionRecs(Cell_getCollisionRect(&cells[i].as_Cell), Cell_getCollisionRect(&pointer));
+			bool collision = CheckCollisionRecs(Box_getCollisionRect(&cells[i].as_Box), Box_getCollisionRect(&pointer));
 			if (!selected_cell)
 			{
-				Cell_Selectable_setSelected(&cells[i], collision);
+				Cell_setSelected(&cells[i], collision);
 				if (collision)
 				{
 					selected_cell = &cells[i];
@@ -264,11 +264,11 @@ int main(void)
 			}
 			else if (i != selected_cell->idx)
 			{
-				Cell_Selectable_setSelected(&cells[i], false);
+				Cell_setSelected(&cells[i], false);
 			}
 			else // if (selected_cell && i == selected_cell->idx)
 			{
-				Cell_Selectable_setSelected(&cells[i], collision);
+				Cell_setSelected(&cells[i], collision);
 				if (!collision)
 				{
 					selected_cell = 0;
@@ -285,14 +285,14 @@ int main(void)
 			{
 				for (int j = 0; j < CELLS_COUNT; ++j)
 				{
-					if (i != j && Cell_Selectable_AreCellsConnected(&cells[i], &cells[j]))
+					if (i != j && Cell_AreCellsConnected(&cells[i], &cells[j]))
 					{
-						DrawLineV(cells[i].as_Cell.pos, cells[j].as_Cell.pos, BLACK);
-						Vector2 diff = Vector2Subtract(cells[j].as_Cell.pos, cells[i].as_Cell.pos);
+						DrawLineV(cells[i].as_Box.pos, cells[j].as_Box.pos, BLACK);
+						Vector2 diff = Vector2Subtract(cells[j].as_Box.pos, cells[i].as_Box.pos);
 						Vector2 direction = Vector2Normalize(diff);
 						float length = Vector2Length(diff);
 						float speed = 50.0;
-						Vector2 pixel_pos = Vector2Add(cells[i].as_Cell.pos, Vector2Scale(direction, fmod(GetTime() * speed, length)));
+						Vector2 pixel_pos = Vector2Add(cells[i].as_Box.pos, Vector2Scale(direction, fmod(GetTime() * speed, length)));
 						DrawRectangleRec(Rect_CreateCentered(pixel_pos, (Vector2) {.x = 5, .y = 5}), BLACK);
 					}
 				}
@@ -304,23 +304,23 @@ int main(void)
 				{
 					if (!selected_cell || (selected_cell && selected_cell->idx != i))
 					{
-						Cell_draw(&cells[i].as_Cell);
+						Box_draw(&cells[i].as_Box);
 					}
 				}
 
 				// Draw selected cells over others
 				if (selected_cell)
 				{
-					Cell_draw(&selected_cell->as_Cell);
+					Box_draw(&selected_cell->as_Box);
 				}
 			}
 
 			if (connecting_cell)
 			{
-				DrawLineV(connecting_cell->as_Cell.pos, pointer.pos, WHITE);
+				DrawLineV(connecting_cell->as_Box.pos, pointer.pos, WHITE);
 			}
 
-			Cell_draw(&pointer);
+			Box_draw(&pointer);
 		}
 		EndDrawing();
 	}
